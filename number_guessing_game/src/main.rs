43eba7d01ead3bb29eff4fc4155ed5a515ab21d3
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-08-08 15:43:04 +0300

    Integrate main game loop

--- number_guessing_game/src/main.rs
+++ number_guessing_game/src/main.rs
@@ -1,16 +1,27 @@
 use std::io::{self, Write};
+use std::cmp::Ordering;
 use rand::Rng;
 
 fn main() {
     let random_number: i32 = rand::thread_rng().gen_range(1..=100); 
     println!("Guess numbers until you can guess the correct one!");
     
-    print!("Your guess: ");
-    io::stdout().flush().unwrap();
+    loop {
+        print!("Your guess: ");
+        io::stdout().flush().expect("How did this error even happen?");
 
-    let mut guess: String = String::new();
+        let mut guess: String = String::new();
+        io::stdin().read_line(&mut guess).expect("Could not read your line!");
+        let guess: i32 = guess.trim().parse().expect("You must only enter numbers!"); 
 
-    io::stdin().read_line(&mut guess).expect("Can only accept a number!");
-
-    println!("You guessed {guess}")
+        match guess.cmp(&random_number) {
+            Ordering::Less => println!("Your number was less than the secret number."),
+            Ordering::Greater => println!("Your number was greater than the secret number"),
+            Ordering::Equal => {
+                println!("Spot on!");
+                break;
+            }
+        }
+        
+    }
 }
