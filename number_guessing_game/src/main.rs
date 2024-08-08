3ba562332fbb98e4aabcd3a974fba0349b519830
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-08-08 15:32:06 +0300

    Feat user input reading

--- number_guessing_game/src/main.rs
+++ number_guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io::{self, Write};
+
 fn main() {
-    
+    println!("Guess numbers until you can guess the correct one!");
+    print!("Your guess: ");
+    io::stdout().flush().unwrap();
+
+    let mut guess: String = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Can only accept a number!");
+
+    println!("You guessed {guess}")
 }
