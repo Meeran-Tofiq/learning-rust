ac8c401fb17d6c8a09cc18db0f9de61fb9c13801
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-08-11 10:35:47 +0300

    Feat ownership basics experimentation

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -1,3 +1,37 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s: String = String::from("Hello");
+    s += " World";
+
+    {
+        let m: String = String::from("NO WAY");
+        s = m;
+        // println!("{m}"); // Cannot use this m because it's invalid now
+    }
+
+    takes_onwership(s);
+    // println!("{s}"); // Cannot use this because s's value has changed owners
+
+    let x: i32 = 23;
+    makes_copy(x);
+    println!("Number is {x}");
+
+    let (str, len): (String, usize) = calculate_length(String::from("Hola, soy Dora!"));
+
+    println!("Str is {str}, and length is {len}")
 }
+
+fn takes_onwership(some_string: String) {
+    println!("String is {some_string}");
+}
+
+fn makes_copy(num: i32) {
+    println!("Number is {num}");
+}
+
+fn calculate_length(str: String) -> (String, usize) {
+    let length: usize = str.len();
+
+    println!("Length is {length}");
+
+    (str, length)
+} 
