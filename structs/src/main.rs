40f9d9ea961515e8524847bf7f2dd2edd168ea1e
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-08-13 10:48:10 +0300

    Feat struct basics

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,32 @@
+struct User {
+    first_name: String,
+    last_name: String,
+    age: i8,
+    active: bool,
+    email: String
+}
+
+fn create_user(first_name: String, last_name: String, age: i8, email: String) -> User {
+    User {
+        first_name,
+        last_name,
+        age,
+        active: true,
+        email
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user_a = create_user(
+        String::from("Meran"), 
+        String::from("Tofiq"), 
+        22, 
+        "meran.saman2310@gmail.com".to_string());
+
+    let user_b = User {
+        email: "noway@gmail.com".to_string(),
+        ..user_a
+    };
+
+    println!("{}", user_b.first_name);
 }
