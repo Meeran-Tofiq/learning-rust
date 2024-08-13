49f8269cd12688652d6b394ac2286142cb3d2ff7
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-08-13 21:02:40 +0300

    Feat basic enum matching

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -4,9 +4,36 @@ enum IpAddrKind {
     V6(String)
 }
 
+#[derive(Debug)]
+enum UsState {
+    Alaska,
+    Arkansas
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState)
+}
+
 fn main() {
     let home_ip = IpAddrKind::V4("127.0.0.1".to_string());
     let office_ip = IpAddrKind::V6("::1".to_string());
+    let coin: Coin = Coin::Quarter(UsState::Alaska);
 
     println!("{:?}", home_ip);
+    filter_coin(&coin);
+}
+
+fn filter_coin(coin: &Coin) -> i32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("The state of the coin is {state:?}");
+            25
+        }
+    }
 }
