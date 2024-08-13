442f705628333440c49d9fbcdd875b6bb7f6b0e7
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-08-13 20:54:14 +0300

    Feat basic enums

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,12 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4(String),
+    V6(String)
+}
+
 fn main() {
-    println!("Hello, world!");
+    let home_ip = IpAddrKind::V4("127.0.0.1".to_string());
+    let office_ip = IpAddrKind::V6("::1".to_string());
+
+    println!("{:?}", home_ip);
 }
