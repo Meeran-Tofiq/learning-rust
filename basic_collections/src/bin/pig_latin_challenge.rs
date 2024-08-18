80fa3ce6c5649b49fe4cfb11a9f767c071caba3b
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-08-18 13:28:58 +0300

    Feat pig latin transformation

--- basic_collections/src/bin/pig_latin_challenge.rs
+++ basic_collections/src/bin/pig_latin_challenge.rs
@@ -1,4 +1,4 @@
-use std::io;
+use std::io::{self, Write};
 
 const VOWELS: [&str; 5] = ["a", "e", "i", "o", "u"];
 const SUFFIXES: [&str; 2] = ["ay", "hay"];
@@ -18,12 +18,28 @@ fn check_first_letter(word: &str) -> FirstLetterState {
     FirstLetterState::Consonant
 }
 
+fn transform_consonanted_word(word: &str) -> String {
+    let first_letter = &word[0..1];
+    let word_remainder = &word[1..];
+    return format!("{}-{}{}", word_remainder, first_letter, SUFFIXES[0]);
+}
+
+fn transform_voweled_word(word: &str) -> String {
+    return format!("{}-{}", word, SUFFIXES[1]);
+}
+
 fn main(){
+    print!("Your word to be transformed into pig latin: ");
+    io::stdout().flush().expect("How did this error even happen?");
+
     let mut word: String = String::new();
     io::stdin().read_line(&mut word).expect("Could not read your word");
+    word = word.trim().to_string();
 
-    match check_first_letter(&word) {
-        FirstLetterState::Consonant => println!("Consonant"),
-        FirstLetterState::Vowel => println!("Vowel")
-    }
+    let result: String = match check_first_letter(&word) {
+        FirstLetterState::Consonant => transform_consonanted_word(&word),
+        FirstLetterState::Vowel =>  transform_voweled_word(&word)
+    };
+
+    println!("{word} in pig latin is {result}");
 }
