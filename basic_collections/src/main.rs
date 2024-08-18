e4cd4a068b5e8b538e450a92370bf6b272804959
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-08-18 11:55:09 +0300

    Feat hashmap experiments

--- basic_collections/src/main.rs
+++ basic_collections/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut x: Vec<i32> = Vec::new();
     x.push(1);
@@ -29,4 +31,17 @@ fn main() {
     let t3: String = String::from("toe");
     let t = format!("{}-{}-{}", t1, t2, t3);
     println!("{t}");
+
+    let mut map: HashMap<String, i32> = HashMap::new();
+    map.insert("Banana".to_string(), 99);
+    map.insert("Apples".to_string(), 73);
+    map.insert("Oranges".to_string(), 112);
+    println!("{:?}", map);
+    let banana_sales = map.get("Banana").copied().unwrap_or(-1);
+    let melon_sales = map.get("Melon").copied().unwrap_or(-1);
+    println!("{}", banana_sales);
+    println!("{}", melon_sales);
+    let j = map.entry("Strawberry".to_string()).or_insert(999).clone();
+    println!("{:?}", map);
+    println!("Stawberries is {}", j);
 }
