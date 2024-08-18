163ef03c229bdb9575c005012c949a07aef41206
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-08-18 09:14:59 +0300

    Feat vector experiments

--- basic_collections/src/main.rs
+++ basic_collections/src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let mut x: Vec<i32> = Vec::new();
+    x.push(1);
+    x.push(3);
+    x.push(5);
+
+    println!("The number is {}", x[2]);
+
+    x.push(99);
+
+    match x.get(3) {
+        Some(val) => println!("The fourth number is {}", val),
+        None => println!("There is no number at the fourth spot.") 
+    }
+
+    for i in &x {
+        print!("{}\t", i);
+    }
+
+    println!("Vector is {:?}", x);
 }
