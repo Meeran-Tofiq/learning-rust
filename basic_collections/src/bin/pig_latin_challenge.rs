e3b40621f3a21f63ac896d082208475833c2c210
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-08-18 13:09:35 +0300

    Feat first letter checker

--- basic_collections/src/bin/pig_latin_challenge.rs
+++ basic_collections/src/bin/pig_latin_challenge.rs
@@ -1,3 +1,24 @@
+use std::io;
+
+const VOWELS: [&str; 5] = ["a", "e", "i", "o", "u"];
+const SUFFIXES: [&str; 2] = ["ay", "hay"];
+
+enum FirstLetterState {
+    Vowel,
+    Consonant
+}
+
+fn check_first_letter(word: &str) -> FirstLetterState {
+    let first_letter = &word[0..1];
+
+    if VOWELS.contains(&first_letter) {
+        return FirstLetterState::Vowel;
+    }
+
+    FirstLetterState::Consonant
+}
+
 fn main(){
-    
+    let mut word: String = String::new();
+    io::stdin().read_line(&mut word).expect("Could not read your word");
 }
