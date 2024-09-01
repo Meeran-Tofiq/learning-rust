346dfd33c6d4a3b3fc08686f5c613402ea7745e0
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-01 10:31:59 +0300

    Feat error handling exp

--- error_handling/src/main.rs
+++ error_handling/src/main.rs
@@ -1,3 +1,15 @@
+use std::fs::File;
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let meh = read_username_from_file();
+    println!("{meh:?}");
+    let greeting_file = File::open("hello.txt")
+        .expect("hello.txt should be included in this project");
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut username = String::new();
+    File::open("hello.txt")?.read_to_string(&mut username)?;
+    Ok(username)
 }
