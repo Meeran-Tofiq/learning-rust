cb96c86f3740b08fa898c2c816bc231ccd610989
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-02 11:37:51 +0300

    Feat trait experiments

--- generics_traits_lifetimes/src/main.rs
+++ generics_traits_lifetimes/src/main.rs
@@ -1,3 +1,35 @@
+pub trait Summary {
+    fn summarize(&self) -> String {
+        String::from("(Read more...)")
+    }
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{} {}: {}", self.location, self.author, self.headline)
+    }
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!{"{}: {}", self.username, self.content}
+    }
+}
+
 struct Point<T> {
     x: T,
     y: T
