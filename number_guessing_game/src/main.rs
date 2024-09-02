7bee543746f93405fcd8dc40b6bd4e0795d96926
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-02 10:41:13 +0300

    Refactor to use Guess struct

--- number_guessing_game/src/main.rs
+++ number_guessing_game/src/main.rs
@@ -2,6 +2,9 @@ use std::io::{self, Write};
 use std::cmp::Ordering;
 use rand::Rng;
 
+mod guess;
+use guess::Guess;
+
 fn main() {
     let random_number: i32 = rand::thread_rng().gen_range(1..=100); 
     println!("Guess numbers until you can guess the correct one!");
@@ -12,20 +15,15 @@ fn main() {
 
         let mut guess: String = String::new();
         io::stdin().read_line(&mut guess).expect("Could not read your line!");
-        let guess: i32 = match guess.trim().parse() {
-            Ok(val) => val,
+        let guess: Guess = match guess.trim().parse() {
+            Ok(val) => Guess::new(val),
             Err(err) => {
                 println!("{:?}", err.to_string());
                 continue;
             }
         };
 
-        if guess < 1 || guess > 100 {
-            println!("The number will always be between 1 and 100.");
-            continue;
-        }
-
-        match guess.cmp(&random_number) {
+        match guess.value().cmp(&random_number) {
             Ordering::Less => println!("Your number was less than the secret number."),
             Ordering::Greater => println!("Your number was greater than the secret number"),
             Ordering::Equal => {
