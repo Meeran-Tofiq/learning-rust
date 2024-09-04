b1c013b01118b41bfcfe046b41ba112ae2d42dfa
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-04 10:54:40 +0300

    Feat experiments with differing lifetimes

--- generics_traits_lifetimes/src/bin/lifetimes.rs
+++ generics_traits_lifetimes/src/bin/lifetimes.rs
@@ -2,8 +2,18 @@ fn main() {
     let string1: String = String::from("abcd");
     let string2: &str = "xyz";
 
-    let result = longest(string1.as_str(), string2);
+    let mut result = longest(string1.as_str(), string2);
     println!("The longest string slice is {result}");
+
+    let string1 = String::from("long string is long");
+
+    {
+        let string2 = String::from("xyz");
+        result = longest(string1.as_str(), string2.as_str());
+        println!("The longest string is {result}");
+    }
+
+    // println!("{result}"); this line produces an error
 }
 
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
