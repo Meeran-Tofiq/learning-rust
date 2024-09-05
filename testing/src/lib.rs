6eba150e1d7f7f32197474561c9f0c5d82da93bf
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-05 09:31:55 +0300

    Feat rectangle struct

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -1,3 +1,15 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
