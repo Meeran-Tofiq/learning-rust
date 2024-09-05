8bb8aea0f8dee4a826762ce13258ba7d86b052f6
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-05 09:31:40 +0300

    Feat basic testing experi.

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -11,4 +11,15 @@ mod tests {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+    #[test]
+    fn it_works_again() {
+        let result = add(2, 2);
+        assert_ne!(result, 99);
+    }
+
+    #[test]
+    fn it_fails() {
+        panic!("at the disco");
+    }
 }
