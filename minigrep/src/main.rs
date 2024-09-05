c96b5c16a2b86251ee2e2f81d902e1a3262eb671
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-05 14:31:57 +0300

    Feat reading file data

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,9 +1,19 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let query = &args[1];
-    let file_path = &args[2];
-
     
+    let [_, query, file_path] = &args[..] else {
+        eprintln!("Usage: <program> <query> <file>");
+        return;
+    };
+
+    println!("Searching for {}...", query);
+    println!("Within {}", file_path);
+
+    let content = fs::read_to_string(file_path)
+    .expect("Couldn't read file data...");
+
+    println!("With text content of:\n{}", content)
 }
