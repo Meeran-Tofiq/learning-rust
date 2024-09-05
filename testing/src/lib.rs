2e56e2e7f20fa0aafc7ad3878b884bf14e519247
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-05 09:37:59 +0300

    Test rectangle can_hold method

--- testing/src/lib.rs
+++ testing/src/lib.rs
@@ -30,8 +30,37 @@ mod tests {
         assert_ne!(result, 99);
     }
 
+    // #[test]
+    // fn it_fails() {
+    //     panic!("at the disco");
+    // }
+
+    fn get_two_rectangles() -> (Rectangle, Rectangle) {
+        let rect_1: Rectangle = Rectangle {
+            width: 43,
+            height: 22,
+        };
+        let rect_2: Rectangle = Rectangle {
+            width: 40,
+            height: 12,
+        };
+
+        (rect_1, rect_2)
+    }
+
     #[test]
-    fn it_fails() {
-        panic!("at the disco");
+    fn larger_rectangle_can_hold_smaller() {
+        let (rect_1, rect_2): (Rectangle, Rectangle) = get_two_rectangles();
+        let can_hold = rect_1.can_hold(&rect_2);
+
+        assert!(can_hold);
+    }
+
+    #[test]
+    fn smaller_rectangle_cannot_hold_larger() {
+        let (rect_1, rect_2): (Rectangle, Rectangle) = get_two_rectangles();
+        let can_hold = rect_2.can_hold(&rect_1);
+
+        assert!(!can_hold);
     }
 }
