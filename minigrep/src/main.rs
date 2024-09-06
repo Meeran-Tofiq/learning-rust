be5b92fce4f22a709c26b9fc387ff8bbddb82ffd
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-06 19:35:52 +0300

    Feat error handling in the Config constructor

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let config: Config = Config::new(&args);
+    let config: Config = Config::build(&args).unwrap_or_else(|err| {
+        println!("{err}");
+        process::exit(1);
+    });
 
     println!("Searching for {}...", config.query);
     println!("Within {}", config.file_path);
@@ -21,10 +25,14 @@ pub struct Config {
 }
 
 impl Config {
-    fn new(args: &Vec<String>) -> Self {
+    fn build(args: &Vec<String>) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Usage: <command> <query> <file_path>");
+        }
+
         let query = args[1].clone();
         let file_path = args[2].clone();
     
-        Self { query, file_path}
+        Ok(Self { query, file_path})
     }
 }
