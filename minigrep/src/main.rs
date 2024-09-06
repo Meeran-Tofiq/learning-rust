6089b609c07d7a012642d1d8442d6c4591fa1006
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-06 22:35:51 +0300

    Feat colored print messages on error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,13 +1,14 @@
 use std::env;
 use std::process;
 
+use colored::Colorize;
 use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     
     let config: Config = Config::build(&args).unwrap_or_else(|err| {
-        eprintln!("{err}");
+        eprintln!("{}", err.red());
         process::exit(1);
     });
 
@@ -15,7 +16,7 @@ fn main() {
     println!("Within {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        eprintln!("Application error: {e}");
+        eprintln!("{}", format!("Application error: {e}").red());
         process::exit(1);
     }
 }
