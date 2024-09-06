17f42ef26e610167071b4b9bbf20e309ae708aba
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-06 13:23:23 +0300

    Feat struct Config to handle config parameters

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,20 +4,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
     
-    let (query, file_path): (&str, &str) = parse_config(&args);
+    let config: Config = parse_config(&args);
 
-    println!("Searching for {}...", query);
-    println!("Within {}", file_path);
+    println!("Searching for {}...", config.query);
+    println!("Within {}", config.file_path);
 
-    let content = fs::read_to_string(file_path)
+    let content = fs::read_to_string(config.file_path)
     .expect("Couldn't read file data...");
 
     println!("With text content of:\n{}", content)
 }
 
-fn parse_config(args: &Vec<String>) -> (&str, &str) {
-    let query = &args[1];
-    let file_path = &args[2];
+pub struct Config {
+    query: String,
+    file_path: String,
+}
 
-    (query, file_path)
+fn parse_config(args: &Vec<String>) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path}
 }
