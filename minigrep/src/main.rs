2f783f3197ab370f1b9aefb777799ab25789a371
Meeran Tofiq <101665499+Meeran-Tofiq@users.noreply.github.com>
2024-09-06 19:39:16 +0300

    Feat run func to take some of main's functionality

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -13,10 +13,14 @@ fn main() {
     println!("Searching for {}...", config.query);
     println!("Within {}", config.file_path);
 
+    run(config)
+}
+
+fn run(config: Config) {
     let content = fs::read_to_string(config.file_path)
     .expect("Couldn't read file data...");
 
-    println!("With text content of:\n{}", content)
+    println!("With text content of:\n{content}");
 }
 
 pub struct Config {
